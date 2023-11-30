af625647870e44e97abcc4f6b23ddf37c43e1830
ChillyForest <1394466835@qq.com>
2023-11-30 16:13:10 +0800

    feat: cpu exceptions

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
+#![feature(abi_x86_interrupt)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
 use core::panic::PanicInfo;
@@ -38,9 +40,11 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     loop {}
 }
 
+/// Entry point for `cargo test`
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
@@ -64,4 +68,8 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
     }
+}
+
+pub fn init() {
+    interrupts::init_idt();
 }
