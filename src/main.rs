2168b8bfd7020ef6d047f1e5f3160af7ce93eca6
ChillyForest <1394466835@qq.com>
2023-12-26 08:43:34 +0800

    feat: heap allocation

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,19 @@
 
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use xm_os::{
-    memory::{self, BootInfoFrameAllocator},
-    println,
-};
+use x86_64::VirtAddr;
+use xm_os::println;
 
+extern crate alloc;
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 // #[no_mangle]
 // pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {}
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use x86_64::{structures::paging::Page, VirtAddr};
+    use xm_os::allocator;
+    use xm_os::memory::{self, BootInfoFrameAllocator};
 
     println!("Hello World{}", "!");
     xm_os::init();
@@ -26,13 +27,29 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe {
-        page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e);
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
     }
+    println!("vec at {:p}", vec.as_slice());
+
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
+
     #[cfg(test)]
     test_main();
 
